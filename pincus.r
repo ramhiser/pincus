5764fa27ddd17400102335a18ca0984496354ddf
John Ramey <johnramey@gmail.com>
2011-05-20 17:28:46 -0500

    Pincus is almost finished. Need to calculate the total number of combinations on-the-fly.

--- pincus.r
+++ pincus.r
@@ -1,23 +1,33 @@
 library('foreach')
 library('itertools')
+library('Brobdingnag')
 
 grid_size <- 10
 chunk_size <- 5
 beta1 <- beta2 <- beta3 <- beta4 <- seq(0, 1, length = grid_size)
 
-# TODO: Change pincus_integral() to actual formula.
-pincus_integral <- function(x) {
-	x
+pincus_formula <- function(x, fn, lambda = 10) {
+	colSums(exp(lambda * x * fn(x)) / exp(lambda * fn(x)))
 }
 
-pincus <- function(..., chunk_size = 10) {
+pincus <- function(..., fn, lambda, chunk_size = 10) {
 	iter_chunk <- ichunk(product(...), chunkSize = chunk_size)
-	out <- foreach(chunk = iter_chunk, .combine=rbind) %do% {
-		x <- do.call(rbind, lapply(chunk, function(x) x))	
-		pincus_integral(x)
+	pincus_sums <- foreach(chunk = iter_chunk, .combine=rbind) %do% {
+		x <- do.call(rbind, lapply(chunk, function(x) unlist(x)))
+		pincus_formula(x, fn = fn, lambda = lambda)
 	}
-	out
+	# TODO: Divide by the total number of combinations
+	colSums(pincus_sums) / 10000
 }
 
-(pincus_out <- pincus(beta1, beta2, beta3, chunk_size = chunk_size))
+#(pincus_out <- pincus(beta1, beta2, beta3, chunk_size = chunk_size))
 #(pincus_out <- pincus(beta1, beta2, beta3, beta4, chunk_size = 10000))
+
+
+f <- function(x) {
+	sin(sqrt(x[,1]^2 + x[,2]^2)) / sqrt(x[,1]^2 + x[,2]^2) - sqrt(x[,1]^2 + x[,2]^2)
+}
+x <- y <- seq(-5, 5, length = 100)
+pincus.out <- pincus(x, y, fn = f, lambda = 10, chunk_size = 1000)
+
+print(pincus.out)
