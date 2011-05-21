07ae27efe32da67feb0908530e35b343935db6ae
John Ramey <johnramey@gmail.com>
2011-05-20 20:53:32 -0500

    Updated the Pincus sim.

--- pincus.r
+++ pincus.r
@@ -2,32 +2,34 @@ library('foreach')
 library('itertools')
 library('Brobdingnag')
 
-grid_size <- 10
-chunk_size <- 5
-beta1 <- beta2 <- beta3 <- beta4 <- seq(0, 1, length = grid_size)
 
+# The actual formula to compute the function, fn's, optimum
 pincus_formula <- function(x, fn, lambda = 10) {
 	colSums(exp(lambda * x * fn(x)) / exp(lambda * fn(x)))
 }
 
+# ...: parameter grids
+# fn: function to optimize
+# lambda: constant that should be large to use the Pincus theorem
+# chunk_size: Size of subset of Cartesian product of grid.
 pincus <- function(..., fn, lambda, chunk_size = 10) {
 	iter_chunk <- ichunk(product(...), chunkSize = chunk_size)
-	pincus_sums <- foreach(chunk = iter_chunk, .combine=rbind) %do% {
-		x <- do.call(rbind, lapply(chunk, function(x) unlist(x)))
-		pincus_formula(x, fn = fn, lambda = lambda)
+	out <- foreach(chunk = iter_chunk)  %dopar% {
+		grid <- do.call(rbind, lapply(chunk, function(x) unlist(x)))
+		list(
+			pincus = pincus_formula(grid, fn = fn, lambda = lambda),
+			count = nrow(grid)
+		)
 	}
-	# TODO: Divide by the total number of combinations
-	colSums(pincus_sums) / 10000
+	col_sums <- do.call(rbind, lapply(out, function(x) x$pincus))
+	count <- do.call(rbind, lapply(out, function(x) x$count))
+	colSums(col_sums) / sum(count)
 }
-
-#(pincus_out <- pincus(beta1, beta2, beta3, chunk_size = chunk_size))
-#(pincus_out <- pincus(beta1, beta2, beta3, beta4, chunk_size = 10000))
-
 
 f <- function(x) {
 	sin(sqrt(x[,1]^2 + x[,2]^2)) / sqrt(x[,1]^2 + x[,2]^2) - sqrt(x[,1]^2 + x[,2]^2)
 }
 x <- y <- seq(-5, 5, length = 100)
-pincus.out <- pincus(x, y, fn = f, lambda = 10, chunk_size = 1000)
+pincus_out <- pincus(x, y, fn = f, lambda = 10, chunk_size = 1000)
 
-print(pincus.out)
+print(pincus_out)
